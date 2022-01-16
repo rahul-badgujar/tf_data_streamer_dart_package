d3b0f0f37a1fc1eb398f0aa0b93b9f6397282cf7
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-01-17 05:09:56 +0530

    added example

--- example/tf_data_streamer_example.dart
+++ example/tf_data_streamer_example.dart
@@ -3,6 +3,9 @@ import 'dart:math';
 
 import 'package:tf_data_streamer/tf_data_streamer.dart';
 
+/// This example shows how the TfDataStreamer can be extended and implemented for an Auth Listener.
+
+/// Represents various Auth Status
 enum AuthStatus {
   signedIn,
   signedOut,
@@ -10,18 +13,24 @@ enum AuthStatus {
   restricted,
 }
 
+/// Returns the AuthStatus from Server. The values are faked by randomly selecting any for example.
 AuthStatus getFakeAuthStatusFromServer() {
   final authStatuses = AuthStatus.values;
   final index = Random().nextInt(authStatuses.length + 1);
+  // error case
   if (index >= authStatuses.length) {
     throw Exception('Error');
   }
   return authStatuses[index];
 }
 
+/// The AuthListener Implementation
 class AuthListener extends TfDataStreamer<AuthStatus> {
+  // To store the current auth status, this data will be streamed.
+  // The data being streamed should match the template argument type.
   late AuthStatus _currentAuthStatus;
 
+  /// Getter for currentAuthStatus
   AuthStatus get currentAuthStatus {
     return _currentAuthStatus;
   }
@@ -29,24 +38,31 @@ class AuthListener extends TfDataStreamer<AuthStatus> {
   @override
   void reload() {
     try {
+      // get the current auth status from server
       _currentAuthStatus = getFakeAuthStatusFromServer();
+      // stream the new event for listeners to listen to
       addData(currentAuthStatus);
     } on Exception catch (e) {
+      // in case of error, stream the error to listeners
       addError(e);
     }
   }
 }
 
 void main() {
+  // Instantiate and initialize the AuthListener
   final authStatusListener = AuthListener();
   authStatusListener.init();
 
+  // Listen to the stream of data update events
   authStatusListener.stream.listen((event) {
     print('Auth Status Changed to $event');
   }, onError: (error) {
     print('Failed to get latest Auth Status.');
   });
 
+  // For the instance, the server will be pinged every 3 seconds for latest auth status.
+  // We can add data, add error, or reload() from anywhere in the code, and listeners will listen to it.
   Timer.periodic(Duration(seconds: 3), (timer) {
     authStatusListener.reload();
   });
