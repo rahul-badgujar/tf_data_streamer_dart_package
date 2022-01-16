7f57b231bedeaa0e7d00ed6b1ba4df347e0af861
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-01-16 06:55:55 +0530

    few tweeks

--- lib/tf_data_streamer.dart
+++ lib/tf_data_streamer.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library tf_data_streamer;
 
 export 'src/tf_data_streamer_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
