32d0cfd0e0ad44fb65b15fb70d545ea7f64082d4
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-01-16 06:54:30 +0530

    init

---
+++ example/tf_data_streamer_example.dart
@@ -0,0 +1,3 @@
+import 'package:tf_data_streamer/tf_data_streamer.dart';
+
+void main() {}
---
+++ lib/tf_data_streamer.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library tf_data_streamer;
+
+export 'src/tf_data_streamer_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
